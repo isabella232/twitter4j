08ff5fb93244a18d02d989857ecc618b87b6ac54
cs19 <>
2019-07-12 12:07:15 -0400

    dont do expensive SSLContext generation on each request

--- twitter4j-core/src/internal-http/java/twitter4j/HttpClientBase.java
+++ twitter4j-core/src/internal-http/java/twitter4j/HttpClientBase.java
@@ -1,30 +1,55 @@
 package twitter4j;
 
-import twitter4j.auth.Authorization;
-
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+
+import twitter4j.auth.Authorization;
 
 public abstract class HttpClientBase implements HttpClient, Serializable {
     private static final Logger logger = Logger.getLogger(HttpClientBase.class);
     private static final long serialVersionUID = -8016974810651763053L;
+
     protected final HttpClientConfiguration CONF;
 
     private final Map<String, String> requestHeaders;
+    private final SSLSocketFactory sslSocketFactory;
 
     public HttpClientBase(HttpClientConfiguration conf) {
         this.CONF = conf;
-        requestHeaders = new HashMap<String, String>();
-        requestHeaders.put("X-Twitter-Client-Version", Version.getVersion());
-        requestHeaders.put("X-Twitter-Client-URL", "http://twitter4j.org/en/twitter4j-" + Version.getVersion() + ".xml");
-        requestHeaders.put("X-Twitter-Client", "Twitter4J");
-        requestHeaders.put("User-Agent", "twitter4j http://twitter4j.org/ /" + Version.getVersion());
+
+        this.requestHeaders = new HashMap<>();
+        this.requestHeaders.put("X-Twitter-Client-Version", Version.getVersion());
+        this.requestHeaders.put("X-Twitter-Client-URL", "http://twitter4j.org/en/twitter4j-" + Version.getVersion() + ".xml");
+        this.requestHeaders.put("X-Twitter-Client", "Twitter4J");
+        this.requestHeaders.put("User-Agent", "twitter4j http://twitter4j.org/ /" + Version.getVersion());
         if (conf.isGZIPEnabled()) {
-            requestHeaders.put("Accept-Encoding", "gzip");
+            this.requestHeaders.put("Accept-Encoding", "gzip");
         }
+
+        // https://stackoverflow.com/questions/30121510/java-httpsurlconnection-and-tls-1-2
+        // If the KeyManager[] parameter is null, the installed security providers are searched for the highest-priority implementation of the KeyManagerFactory, from which an appropriate KeyManager is obtained.
+        // If the TrustManager[] parameter is null, the installed security providers are searched for the highest-priority implementation of the TrustManagerFactory, from which an appropriate TrustManager is obtained.
+        // Likewise, the SecureRandom parameter can be null, in which case a default implementation is used.
+        try {
+            SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+            sslContext.init(null, null, new java.security.SecureRandom());
+            this.sslSocketFactory = sslContext.getSocketFactory();
+        } catch (KeyManagementException | NoSuchAlgorithmException e) {
+            throw new RuntimeException("Generating TLSv1.2 SSLContext failed", e);
+        }
+
+    }
+
+    protected SSLSocketFactory getTlsSslSocketFactory() {
+        return sslSocketFactory;
     }
 
     protected boolean isProxyConfigured() {
